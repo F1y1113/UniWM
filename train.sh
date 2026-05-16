5a3c9d55beda5a0a1e4392d0f1202093314df610
Yifei Dong <yd2616@columbia.edu>
2026-05-16 00:11:22 -0700

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,3 +1,12 @@
-CUDA_VISIBLE_DEVICES=1 torchrun --nproc_per_node 1 --master_port=20001 train.py \
---model anole --data scand --data_dir ./data --decoder_type anole --image_seq_length 784 --input_format anole --output output --note train --report_to none --do_train --bfloat16
-
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node 1 --master_port=20001 train.py \
+    --model anole \
+    --data scand \
+    --data_dir ./data \
+    --decoder_type anole \
+    --image_seq_length 784 \
+    --input_format anole \
+    --output output \
+    --note train \
+    --report_to none \
+    --do_train \
+    --bfloat16
